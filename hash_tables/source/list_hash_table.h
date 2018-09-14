a24a34232692b4a27619d2ce84661e3291a52f04
feranco <feranco@gmail.com>
2018-09-14 23:21:33 +0200

    add increment to custom iterator

--- hash_tables/source/list_hash_table.h
+++ hash_tables/source/list_hash_table.h
@@ -6,6 +6,7 @@
 #include <iostream>
 using std::find;
 using std::list;
+
 using std::pair;
 using std::vector;
 
@@ -13,7 +14,9 @@ template <typename Key, typename T, typename Hash = std::hash<Key>>
 class HashTable {
  
   using valueType =  pair<const Key, T>;
+  using HashTableType = HashTable<Key, T, Hash>;
   vector<list<valueType>> hashTable;
+  size_t bucketSize = 0;
   size_t size = 0;
   Hash hashFunct;
 
@@ -33,25 +36,75 @@ class HashTable {
     using pointer           = value_type*;
     using reference         = value_type&;
 
+    using listIterator =  typename std::list<valueType>::iterator;
+
     //Constructor
     iterator() { };
+    iterator (HashTableType* ht, listIterator it) : hashTable(ht), lIt(it) {};
+
+    iterator& operator++()
+    {
+      this->goToNextElement();
+      return *this;
+    };
+    /*
+      iterator operator++(int)
+      {
+      size_t old_index = _bucket;
+      this->goto_next_element();
+      return iterator(_map, old_index);
+      };
+    */  
     
   private:
-    void goto_next_element()
+    void goToNextElement()
     {
-      DCHECK_LT_F(_bucket, _map->_num_buckets);
-      do {
-	_bucket++;
-      } while (_bucket < _map->_num_buckets && _map->_states[_bucket] != State::FILLED);
+      Key k = lIt->first;
+      size_t h = this->hashFunct(k);
+      h = h % this->bucketSize();
+      lIt++;
+      if (lIt != this->hashTable[h].end()) {
+	return lIt;
+      }
+
+      for (size_t i = h; i < this->bucketSize(); ++i) {
+	auto it = bucket.begin();
+	if (it != bucket.end()) {
+	  return it;
+	}	  
+      }
+      return iterator(this, hashTable[bucketSize-1].end());
     }
 
+    };
+
+  public:
+    HashTableType* hashTable;
+    listIterator lIt;
   };
+
+  iterator begin()
+  {
+    for (auto bucket : hashTable) {
+      auto it = bucket.begin();
+      if (it != bucket.end()) {
+	return it;
+      }
+    }
+    return iterator(this, hashTable[bucketSize-1].end());
+  }
+
+  iterator end()
+  {
+    return iterator(this, hashTable[bucketSize-1].end());
+  }
   
 
   
   using iterator =  typename std::list<valueType>::iterator;
-    iterator begin() noexcept { return hashTable[0].begin(); }
-    inline iterator end() noexcept { return hashTable[0].end(); }
+  iterator begin() noexcept { return hashTable[0].begin(); }
+  inline iterator end() noexcept { return hashTable[0].end(); }
+  
   HashTable () {}
   //HashTable (hashFunction hf) : hash(hf){}
   
@@ -65,14 +118,14 @@ class HashTable {
 template <typename Key, typename T, typename Hash>
   HashTable<Key,T, Hash>::HashTable(unsigned int size) {
   hashTable.resize(size);
-  this->size = size;
+  bucketSize = size;
 }
 
 
 template <typename Key, typename T, typename Hash>
   typename HashTable<Key, T, Hash>::iterator  HashTable<Key,T, Hash>::search(const Key& k)  {
   auto h =  hashFunct(k);//Hash<T>(k);
-  h = h%size;
+  h = h%bucketSize;
   auto lambda = [&](valueType x){return x.first == k;}; 
   auto it =  find_if(hashTable[h].begin(), hashTable[h].end(),lambda);
   std::cout << "search: " << it->first << std::endl;
@@ -82,10 +135,10 @@ template <typename Key, typename T, typename Hash>
 template <typename Key, typename T, typename Hash>
   typename HashTable<Key,T, Hash>::iterator HashTable<Key,T, Hash>::insert(Key k, T value) {
   auto h = hashFunct(k);
-  h = h%size;
+  h = h%bucketSize;
   std::cout << h << std::endl;
   hashTable[h].push_front(std::make_pair(k,value));
-  //size++;
+  size++;
   return hashTable[h].begin();
 }
 
@@ -95,8 +148,9 @@ template <typename Key, typename T, typename Hash>
 template <typename Key, typename T, typename Hash>
 void HashTable<Key,T, Hash>::remove(typename HashTable<Key,T, Hash>::iterator it) {
    auto h = hashFunct(it->first);
-   h = h%size;
+   h = h%bucketSize;
    hashTable[h].erase(it);
+   size--;
 }
 
 //OVFQEI
